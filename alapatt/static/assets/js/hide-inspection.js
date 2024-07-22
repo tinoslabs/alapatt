d8b59c8e91f102e610611472222439d53d341101
tinoscyberlabs <jincyrajeevanrk@gmail.com>
2024-07-22 14:33:24 +0530

    files commit

---
+++ alapatt/static/assets/js/hide-inspection.js
@@ -0,0 +1,19 @@
+document.onkeydown=function(e)
+{
+    if(event.keyCode == 123)
+    {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode =='I'.charCodeAt(0))
+    {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode =='J'.charCodeAt(0))
+    {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey =='U'.charCodeAt(0))
+    {
+        return false;
+    }
+}
