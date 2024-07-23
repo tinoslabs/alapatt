f17184cabf59dab98decf4897447588f7fbfda3e
tinoscyberlabs <jincyrajeevanrk@gmail.com>
2024-07-23 21:30:20 +0530

    file path commit

--- alapattfashion/settings.py
+++ alapattfashion/settings.py
@@ -51,8 +51,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
 
